693c19c750aa5b614ba5eed5ef3a3da778e4b91f
oscarRodriguez13 <rodriguez.oalejandro@javeriana.edu.co>
2025-05-09 23:04:06 -0500

    Despliegue

---
+++ personapi-dotnet/sql/init.sql
@@ -0,0 +1,90 @@
+-- Primero crear la base de datos si no existe
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'persona_db')
+BEGIN
+    CREATE DATABASE persona_db;
+END
+GO
+
+-- Usar la base de datos
+USE persona_db;
+GO
+
+-- Tabla persona
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'persona')
+BEGIN
+    CREATE TABLE persona (
+        cc INT NOT NULL,
+        nombre VARCHAR(45) NOT NULL,
+        apellido VARCHAR(45) NOT NULL,
+        edad INT NULL,
+        genero CHAR(1) NOT NULL,
+        CONSTRAINT PK__persona__3213666D564B0149 PRIMARY KEY (cc)
+    );
+END
+GO
+
+-- Tabla profesion
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'profesion')
+BEGIN
+    CREATE TABLE profesion (
+        id INT NOT NULL,
+        nom VARCHAR(90) NOT NULL,
+        des TEXT NOT NULL,
+        CONSTRAINT PK__profesio__3213E83F2B1F13F8 PRIMARY KEY (id)
+    );
+END
+GO
+
+-- Tabla telefono
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'telefono')
+BEGIN
+    CREATE TABLE telefono (
+        num VARCHAR(15) NOT NULL,
+        oper VARCHAR(45) NOT NULL,
+        duenio INT NOT NULL,
+        CONSTRAINT PK__telefono__DF908D659694C3B2 PRIMARY KEY (num),
+        CONSTRAINT FK__telefono__duenio__37A5467C FOREIGN KEY (duenio)
+            REFERENCES persona (cc)
+    );
+END
+GO
+
+-- Tabla estudios
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'estudios')
+BEGIN
+    CREATE TABLE estudios (
+        id_prof INT NOT NULL,
+        cc_per INT NOT NULL,
+        fecha DATETIME2 NULL,
+        univer VARCHAR(50) NOT NULL,
+        PersonaCc INT NOT NULL,
+        ProfesionId INT NOT NULL,
+        CONSTRAINT PK__estudios__FB3F71A6B8574F61 PRIMARY KEY (id_prof, cc_per),
+        CONSTRAINT FK__estudios__cc_per__33D4B598 FOREIGN KEY (cc_per)
+            REFERENCES persona (cc),
+        CONSTRAINT FK__estudios__id_pro__34C8D9D1 FOREIGN KEY (id_prof)
+            REFERENCES profesion (id),
+        CONSTRAINT FK_estudios_persona_PersonaCc FOREIGN KEY (PersonaCc)
+            REFERENCES persona (cc) ON DELETE CASCADE,
+        CONSTRAINT FK_estudios_profesion_ProfesionId FOREIGN KEY (ProfesionId)
+            REFERENCES profesion (id) ON DELETE CASCADE
+    );
+END
+GO
+
+-- Creación de índices
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_estudios_cc_per')
+    CREATE INDEX IX_estudios_cc_per ON estudios (cc_per);
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_estudios_PersonaCc')
+    CREATE INDEX IX_estudios_PersonaCc ON estudios (PersonaCc);
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_estudios_ProfesionId')
+    CREATE INDEX IX_estudios_ProfesionId ON estudios (ProfesionId);
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_telefono_duenio')
+    CREATE INDEX IX_telefono_duenio ON telefono (duenio);
+GO
